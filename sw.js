89f6f9d9349a9aeb9a3fac50674e061bdd5c7e4f
djtechcool-create <djtechcool-create@users.noreply.github.com>
2026-07-15 13:53:41 -0500

    Fix PWA start_url and service worker

--- sw.js
+++ sw.js
@@ -1,16 +1,3 @@
-﻿const CACHE = "lotes-v1";
-const ASSETS = ["./", "./visor-lotes.html", "./manifest.json"];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+﻿self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", e => e.waitUntil(clients.claim()));
+self.addEventListener("fetch", () => {});
